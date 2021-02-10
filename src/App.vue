c1c1c50c2e464976c303b952a2bd99eec4369dad
lisonlang <1092037197@qq.com>
2021-02-10 12:23:49 +0800

    改标题

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Homessssssssssssssssssssssss</router-link> |
-      <router-link to="/about">About水水ddddddddddddddddd+分支</router-link>
+      <router-link to="/about">About水水ddddddddddddddddd+分支dddd</router-link>
     </div>
     <router-view/>
   </div>
